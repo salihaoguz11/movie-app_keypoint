e67227b97160c3e4edbe4b52dfce39ec2a4acec7
Saliha Oguz <118985690+salihaoguz11@users.noreply.github.com>
2023-10-30 20:19:28 +0000

    context api updated

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -2,21 +2,22 @@ import { createContext, useEffect, useState } from "react";
 import axios from "axios";
 export const Context = createContext();
 
-const API_KEY = process.env.REACT_APP_apiKey;
-
+//Create ContextProvider component which will warp the application and provide state management
 const ContextProvider = ({ children }) => {
-  const [search, setSearch] = useState("");
-  const [movies, setMovies] = useState([]);
+  const [search, setSearch] = useState(""); // store the current search
+  const [movies, setMovies] = useState([]); // store the data from API
+  const API_KEY = process.env.REACT_APP_apiKey;
   const IMG_URL = "https://image.tmdb.org/t/p/w1280";
   const MULTI_SEARCH_API = `https://api.themoviedb.org/3/search/multi?api_key=${API_KEY}&query=${search}`;
 
+  //create an async function that will be called when the search is done and fetch data from API
   const getData = async () => {
     const { data } = await axios(MULTI_SEARCH_API);
 
     setMovies(data.results);
     console.log(data);
   };
-
+  //create object values for passing down to the childrens
   const values = {
     search,
     setSearch,
