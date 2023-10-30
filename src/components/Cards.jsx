fb8b226ff4637c567e95464e312fed0bb64f5a77
Saliha Oguz <118985690+salihaoguz11@users.noreply.github.com>
2023-10-30 20:37:27 +0000

    card jsx has been updated

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,14 +1,15 @@
-import React, { useContext } from "react";
+import { useContext } from "react";
 import { Container } from "react-bootstrap";
 import { Context } from "../context/Context";
 import MovieCard from "./MovieCard";
 
 const Cards = () => {
-  const { search, setSearch, getData, movies, IMG_URL } = useContext(Context);
+  const { movies, IMG_URL } = useContext(Context);
 
   return (
     <div className="row d-flex ">
       {movies?.map((item) => {
+        //get movies and return each item by using map method
         return (
           <Container className="col-12  col-sm-6 col-md-4 col-lg-3 card mb-4 ms-4 bg-dark">
             <MovieCard {...item} key={item.id} IMG_URL={IMG_URL} />
