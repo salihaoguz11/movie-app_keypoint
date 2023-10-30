48f24dc6c08d6660eb08e256764df43d136f7b8b
Saliha Oguz <118985690+salihaoguz11@users.noreply.github.com>
2023-10-30 15:50:40 +0000

    detail page has improved

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,20 +5,42 @@ import ListGroup from "react-bootstrap/ListGroup";
 import { Context } from "../context/Context";
 
 const MovieCard = ({ movie }) => {
-  const { title, overview, poster_path, id, media_type } = movie;
+  const {
+    title,
+    overview,
+    rate,
+    popularity,
+    name,
+    poster_path,
+    id,
+    media_type,
+    profile_path,
+  } = movie;
+
   const { IMG_URL } = useContext(Context);
   const navigate = useNavigate();
+  const defaultImage =
+    "https://images.unsplash.com/photo-1581905764498-f1b60bae941a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80";
 
   return (
     <Card
       className="movie"
       onClick={() => navigate(`/detail/${media_type}/${id}`)}
     >
-      <Card.Img variant="top" src={IMG_URL + poster_path} />
+      <Card.Img
+        variant="top"
+        src={
+          poster_path
+            ? IMG_URL + poster_path
+            : profile_path
+            ? IMG_URL + profile_path
+            : defaultImage
+        }
+      />
 
       <Card.Footer className="d-flex  justify-content-between p-2 text-white">
-        <Card.Title>{title}</Card.Title>
-        <Card.Title>rate</Card.Title>
+        <Card.Title>{title || name}</Card.Title>
+        <Card.Title>{rate || popularity}</Card.Title>
       </Card.Footer>
     </Card>
   );
--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,11 +1,11 @@
 import React, { useContext, useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { Context } from "../context/Context";
+import SearchBar from "../components/SearchBar";
 import { Container, Row, Col, Image, ListGroup } from "react-bootstrap";
 
 import axios from "axios";
-
-import NavbarCom from "../components/NavbarCom";
+import filmImage from "../helper/img/film.jpg";
 
 const Detail = () => {
   const [detail, setDetail] = useState("");
@@ -24,64 +24,68 @@ const Detail = () => {
     console.log(data);
   };
   return (
-    <Container className="py-5">
-      <h1 className="text-center text-white display-4">
-        {detail.title || detail.name}
-      </h1>
-
-      <Container className="d-flex justify-content-center px-10">
-        <Row className="rounded-lg bg-gray-100 shadow-lg max-w-6xl">
-          <Col lg={4} className="p-0">
-            <Image
-              className="rounded-lg h-100 w-100 object-cover"
-              src={
-                detail.poster_path
-                  ? IMG_URL + detail.poster_path
-                  : detail.profile_path
-                  ? IMG_URL + detail.profile_path
-                  : defaultImage
-              }
-              alt="poster"
-            />
-          </Col>
-          <Col className="p-5 d-flex flex-column justify-content-between">
-            <div>
-              <h5 className="text-gray font-weight-bold mb-2">Overview</h5>
-              <p className="text-gray mb-4">
-                {detail.overview ? detail.overview : "Not Found"}
-              </p>
-            </div>
-            <ListGroup variant="flush">
-              <ListGroup.Item className="rounded-t-lg">
-                {media_type === "person"
-                  ? `Known for:${detail.known_for_department}`
-                  : `Release Date:${detail.release_date}`}
-              </ListGroup.Item>
-              <ListGroup.Item>
-                {" "}
-                {media_type === "person"
-                  ? `Birthday:${detail.birthday}`
-                  : "rate"}
-              </ListGroup.Item>
-              <ListGroup.Item>
-                {" "}
-                {media_type === "person"
-                  ? `Popularity:${detail.popularity}`
-                  : ` Total Vote :  + vote_count`}
-              </ListGroup.Item>
-              <ListGroup.Item className="rounded-t-lg">
-                <Link
-                  to={-1}
-                  className="text-blue-600 hover:text-blue-700 transition duration-300 ease-in-out mb-4"
-                >
-                  Go Back
-                </Link>
-              </ListGroup.Item>
-            </ListGroup>
-          </Col>
-        </Row>
-      </Container>
-    </Container>
+    <div className="min-vh-100 ">
+      <SearchBar />
+      <div className="">
+        <Container className="d-flex justify-content-center ">
+          <Row className="rounded-lg bg-gray-100 shadow-lg max-w-6xl">
+            <Col lg={4} className="p-0">
+              <Image
+                className="rounded-lg h-100 w-100 object-cover"
+                src={
+                  detail.poster_path
+                    ? IMG_URL + detail.poster_path
+                    : detail.profile_path
+                    ? IMG_URL + detail.profile_path
+                    : defaultImage
+                }
+                alt="poster"
+              />
+            </Col>
+            <Col className="p-3 d-flex flex-column justify-content-between">
+              <div>
+                <h2 className="text-center  display-4">
+                  {detail.title || detail.name}
+                </h2>
+                <h5 className="text-gray font-weight-bold mb-2">Overview</h5>
+                <p className="text-gray mb-1">
+                  {detail.overview ? detail.overview : "Not Found"}
+                </p>
+              </div>
+              <ListGroup variant="flush">
+                <ListGroup.Item className="rounded-t-lg">
+                  {media_type === "person"
+                    ? `Known for:${detail.known_for_department}`
+                    : `Release Date:${
+                        detail.release_date || detail.first_air_date
+                      }`}
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  {" "}
+                  {media_type === "person"
+                    ? `Birthday:${detail.birthday}`
+                    : "rate"}
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  {" "}
+                  {media_type === "person"
+                    ? `Popularity:${detail.popularity}`
+                    : ` Total Vote :  + vote_count`}
+                </ListGroup.Item>
+                <ListGroup.Item className="rounded-t-lg">
+                  <Link
+                    to={-1}
+                    className="text-blue-600 hover:text-blue-700 transition duration-300 ease-in-out mb-4"
+                  >
+                    Go Back
+                  </Link>
+                </ListGroup.Item>
+              </ListGroup>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </div>
   );
 };
 
