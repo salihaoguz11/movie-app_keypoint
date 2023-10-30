44fc30b3f8be6436bef70418fd92bd1e0d262e30
Saliha Oguz <118985690+salihaoguz11@users.noreply.github.com>
2023-10-30 15:21:29 +0000

    detail page data fetched

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,6 +1,8 @@
 import React, { useContext, useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { Context } from "../context/Context";
+import { Container, Row, Col, Image, ListGroup } from "react-bootstrap";
+
 import axios from "axios";
 
 import NavbarCom from "../components/NavbarCom";
@@ -8,7 +10,7 @@ import NavbarCom from "../components/NavbarCom";
 const Detail = () => {
   const [detail, setDetail] = useState("");
   const { id, media_type } = useParams();
-  const { API_KEY } = useContext(Context);
+  const { API_KEY, IMG_URL } = useContext(Context);
   const movieDetailBaseUrl = `https://api.themoviedb.org/3/${media_type}/${id}?api_key=${API_KEY}`;
   const defaultImage =
     "https://images.unsplash.com/photo-1581905764498-f1b60bae941a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80";
@@ -22,43 +24,64 @@ const Detail = () => {
     console.log(data);
   };
   return (
-    <div>
-      <NavbarCom />
-      <div className="text-center container">
-        <h2 className="text-danger mt-3 mb-3">title</h2>
+    <Container className="py-5">
+      <h1 className="text-center text-white display-4">
+        {detail.title || detail.name}
+      </h1>
 
-        <div>
-          <img className="w-50" src="image" alt="poster" />
-        </div>
-        <div className="container mt-3">
-          <p>overview</p>
-          <table class="table ">
-            <tbody>
-              <tr>
-                <td colspan="2" class="table-active">
-                  {/* {"Release Date:" + release_date} */}
-                </td>
-              </tr>
-              <tr>
-                <td colspan="2" class="table-active">
-                  {/* {"Rate:" + vote_average} */}
-                </td>
-              </tr>
-              <tr>
-                <td colspan="2" class="table-active">
-                  {/* {"Total Vote:" + vote_count} */}
-                </td>
-              </tr>
-              <tr>
-                <td colspan="2" class="table-active">
-                  <Link to={-1}> Go Back</Link>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </div>
-    </div>
+      <Container className="d-flex justify-content-center px-10">
+        <Row className="rounded-lg bg-gray-100 shadow-lg max-w-6xl">
+          <Col lg={4} className="p-0">
+            <Image
+              className="rounded-lg h-100 w-100 object-cover"
+              src={
+                detail.poster_path
+                  ? IMG_URL + detail.poster_path
+                  : detail.profile_path
+                  ? IMG_URL + detail.profile_path
+                  : defaultImage
+              }
+              alt="poster"
+            />
+          </Col>
+          <Col className="p-5 d-flex flex-column justify-content-between">
+            <div>
+              <h5 className="text-gray font-weight-bold mb-2">Overview</h5>
+              <p className="text-gray mb-4">
+                {detail.overview ? detail.overview : "Not Found"}
+              </p>
+            </div>
+            <ListGroup variant="flush">
+              <ListGroup.Item className="rounded-t-lg">
+                {media_type === "person"
+                  ? `Known for:${detail.known_for_department}`
+                  : `Release Date:${detail.release_date}`}
+              </ListGroup.Item>
+              <ListGroup.Item>
+                {" "}
+                {media_type === "person"
+                  ? `Birthday:${detail.birthday}`
+                  : "rate"}
+              </ListGroup.Item>
+              <ListGroup.Item>
+                {" "}
+                {media_type === "person"
+                  ? `Popularity:${detail.popularity}`
+                  : ` Total Vote :  + vote_count`}
+              </ListGroup.Item>
+              <ListGroup.Item className="rounded-t-lg">
+                <Link
+                  to={-1}
+                  className="text-blue-600 hover:text-blue-700 transition duration-300 ease-in-out mb-4"
+                >
+                  Go Back
+                </Link>
+              </ListGroup.Item>
+            </ListGroup>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
   );
 };
 
