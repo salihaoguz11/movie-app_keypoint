30b94d167d9c9ceaf5338149ccfbad692dd26bdd
Saliha Oguz <118985690+salihaoguz11@users.noreply.github.com>
2023-10-30 08:46:25 +0000

    main page created

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Login from "../pages/Login";
+import Main from "../pages/Main";
 
 const AppRouter = () => {
   return (
     <div>
       <Routes>
         <Route path="login" element={<Login />} />
+        <Route path="/" element={<Main />} />
       </Routes>
     </div>
   );
