4f7fda1f3bd3b7ef144f81bbea5e16b050f1c446
Saliha Oguz <118985690+salihaoguz11@users.noreply.github.com>
2023-10-31 13:48:40 +0000

    app js has been updated

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ import ContextProvider from "./context/Context";
 
 function App() {
   return (
+    // Wrap the entire application in a BrowserRouter to enable routing.
     <BrowserRouter>
+      {/* Provide application-wide context using ContextProvider. */}
       <ContextProvider>
+        {/* Include the main routing logic in AppRouter component. */}
         <AppRouter />
       </ContextProvider>
     </BrowserRouter>
