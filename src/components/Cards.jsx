002a566888de06eadbcb232c6ae1f985d0e4ef2b
Saliha Oguz <118985690+salihaoguz11@users.noreply.github.com>
2023-10-31 11:37:55 +0000

    alertCom has benn added to the cards com

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -2,20 +2,25 @@ import { useContext } from "react";
 import { Container } from "react-bootstrap";
 import { Context } from "../context/Context";
 import MovieCard from "./MovieCard";
+import AlertCom from "./AlertCom";
 
 const Cards = () => {
   const { movies, IMG_URL } = useContext(Context);
 
   return (
     <div className="row d-flex ">
-      {movies?.map((item) => {
-        //get movies and return each item by using map method
-        return (
-          <Container className="col-12  col-sm-6 col-md-4 col-lg-3  mb-4 ms-4    ">
-            <MovieCard {...item} key={item.id} IMG_URL={IMG_URL} />
-          </Container>
-        );
-      })}
+      {movies.length ? (
+        movies.map((item) => {
+          //get movies and return each item by using map method
+          return (
+            <Container className="col-12  col-sm-6 col-md-4 col-lg-3  mb-4 ms-4    ">
+              <MovieCard {...item} key={item.id} IMG_URL={IMG_URL} />
+            </Container>
+          );
+        })
+      ) : (
+        <AlertCom />
+      )}
     </div>
   );
 };
