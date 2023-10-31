f0a12b5805dc5a0fa47f184596d945c5e2e2d83a
Saliha Oguz <118985690+salihaoguz11@users.noreply.github.com>
2023-10-31 09:04:36 +0000

    add some comments

--- src/router/PrivateRouter.jsx
+++ src/router/PrivateRouter.jsx
@@ -4,6 +4,7 @@ import { Context } from "../context/Context";
 
 const PrivateRouter = () => {
   const { user } = useContext(Context);
+  // if there is user you can go to children but if not you navigate to login page.
   return user ? <Outlet /> : <Navigate to="/login" />;
 };
 
