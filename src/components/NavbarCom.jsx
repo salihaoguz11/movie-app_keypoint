946d26d2eb69995bfb06549d3cc5a91fa37d7058
Saliha Oguz <118985690+salihaoguz11@users.noreply.github.com>
2023-10-31 09:42:56 +0000

    navbar has been updated

--- src/components/NavbarCom.jsx
+++ src/components/NavbarCom.jsx
@@ -1,17 +1,26 @@
-import React from "react";
+import { useContext } from "react";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
+import { Context } from "../context/Context";
 
 const NavbarCom = () => {
+  const { user } = useContext(Context);
+  console.log(user);
   return (
     <Navbar bg="dark" data-bs-theme="dark">
       <Container>
         <Navbar.Brand href="#home">Movie App</Navbar.Brand>
         <Navbar.Toggle />
         <Navbar.Collapse className="justify-content-end">
-          <Navbar.Text>
-            Hello, <a href="#login">Mark Otto</a>
-          </Navbar.Text>
+          {user ? (
+            <Navbar.Text>
+              Hello, <a href="#login">{user.email}</a>
+            </Navbar.Text>
+          ) : (
+            <Navbar.Text>
+              Hello, <a href="#login">Mark Otto</a>
+            </Navbar.Text>
+          )}
         </Navbar.Collapse>
       </Container>
     </Navbar>
